b94023794d51a48bf805b9be893311b9dd40d8d2
Gidilounge <gidilounge@Gidilounges-MacBook-Pro.local>
2014-03-11 17:38:23 -0400

    data coming in to client

--- app.js
+++ app.js
@@ -13,6 +13,35 @@ var path = require('path'),
 	io = require('socket.io').listen(server);
 
 
+//MongoDB hookup
+var monk = require('monk');
+var db = function() {
+    return monk('mongodb://lumi:Fibonacci1234@dharma.mongohq.com:10073/gidimongo');
+} 
+
+ var mongoInsertTodo = function(mongoObject) {
+
+
+       var collection = db().get('Todo');
+
+        // Submit to the DB
+        collection.insert(mongoObject, function(err, doc) {
+            if (err) {
+                // If it failed, return error
+                console.log("There was a problem adding the information to the database.");
+            } else {
+                // If it worked, log this
+                console.log("Saved to MongoDB");
+
+
+            }
+        });
+    }
+
+
+
+
+
 server.listen(3000);
 io.set('log level', 1);
 
@@ -42,6 +71,8 @@ app.set('port', process.env.PORT || 8080);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(express.bodyParser());
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -81,9 +112,41 @@ app.get('/cloudafrica', function(req, res) {
         
         res.json(body);
   });
-
-  
 });
 
+app.get('/todo/all', function(req, res){
+  
+  var collection = db().get('Todo');
+   collection.find({}, function (err, docs){
+    
+    if (err) { 
+      console.log(err);
+    } else {
+
+    //console.log(docs);
+      res.json(docs);
+  
+  
+    }
+    
+   });
 
 
+})
+
+
+app.post('/todo', function(req, res) {
+
+  //post to DB
+  mongoInsertTodo(req.body);
+  mongoGetTodos();
+
+    var postdetails = req.body;
+    console.log(req.body);
+    
+    res.json(postdetails);
+
+      
+  
+ 
+  });
